24bc1b149957c299825a4129e3a5313a125a2bea
Andrew Hawker <andrew.r.hawker@gmail.com>
2020-10-01 11:11:17 -0700

    Add microsync package

---
+++ microsync/__init__.py
@@ -0,0 +1,10 @@
+"""
+    microsync
+    ~~~~~~~~~
+
+    Keep your microservice repositories in sync.
+"""
+from . import meta
+
+__version__ = meta.VERSION
+__author__ = meta.AUTHOR
---
+++ microsync/meta.py
@@ -0,0 +1,13 @@
+"""
+    microsync/meta
+    ~~~~~~~~~~~~~~
+
+    Contains information about the package.
+"""
+AUTHOR_NAME = 'Andrew Hawker'
+AUTHOR_EMAIL = 'andrew.r.hawker@gmail.com'
+AUTHOR = 'Andrew Hawker <andrew.r.hawker@gmail.com>'
+NAME = 'microsync'
+VERSION = '0.0.1'
+TAGLINE = 'Keep your microservice repositories in sync.'
+URL = 'https://github.com/microsync/microsync'
