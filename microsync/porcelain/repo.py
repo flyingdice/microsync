354fe53a1d3321022d517c4274fb88a5fc8b6750
Andrew Hawker <andrew.r.hawker@gmail.com>
2020-10-20 14:55:37 -0700

    Add remote_url porcelain function

--- microsync/porcelain/repo.py
+++ microsync/porcelain/repo.py
@@ -8,8 +8,8 @@ from .. import config, defaults, vcs
 from ..hints import OptionalStr, Str
 
 
-def repo(template: Str,
-         path: Str,
+def repo(src: Str,
+         dst: Str,
          ref: OptionalStr = defaults.CLONE_REF,
          options: config.VCS = config.VCS()) -> vcs.Repository:
     """
@@ -18,11 +18,25 @@ def repo(template: Str,
 
     The ref is VCS specific but typically a branch name, commit id/hash, or tag.
 
-    :param template: Source location of template repository
-    :param path: Output file path of cloned repository
+    :param src: Source location of template repository
+    :param dst: Output file path of cloned repository
     :param ref: Version control reference of the source to checkout
     :param options: Options to customize version control system
     :return: Wrapped local repository
     """
     vc = vcs.for_type(options.type)()
-    return vc.get(template, path, ref=ref, options=options)
+    return vc.get(src, dst, ref=ref, options=options)
+
+
+def remote_url(path: Str,
+               options: config.VCS = config.VCS()) -> Str:
+    """
+    Use the user selected version control system to retrieve a code repository
+    remote URL.
+
+    :param path: File path of local repository
+    :param options: Options to customize version control system
+    :return: Remote URL for the local repository
+    """
+    vc = vcs.for_type(options.type)()
+    return vc.repo_url(path)
